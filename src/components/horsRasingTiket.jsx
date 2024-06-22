f10f94ecb5bbea14156feeb76f851e2a4f48aa6b
gizework21 <gizeworkmarye16@gmail.com>
2024-06-22 03:26:26 +0300

    add bet amount on the ticket

--- src/components/horsRasingTiket.jsx
+++ src/components/horsRasingTiket.jsx
@@ -6,7 +6,6 @@ import {
   CardSubtitle,
   ListGroup,
   ListGroupItem,
-
 } from "reactstrap";
 import moment from "moment";
 import { forwardRef } from "react";
@@ -16,15 +15,10 @@ import Barcode from "react-barcode";
 const HorsRasingTiket = forwardRef((props, ref) => {
   const { newBette } = props;
   const [betList, setBetList] = useState([]);
+  const [betAmounts, setBetAmounts] = useState({});
   const [newBetAmount, setNewBetAmount] = useState(0);
-  const [totalBetAmount, setTotalBetAmount] = useState(0); // New state for total bet amount
+  const [totalBetAmount, setTotalBetAmount] = useState(0);
   const [ticketID, setTicketID] = useState('');
-
-  console.log(props.newBette.selectedButtons);
-
-  console.log(betList);
-
-
 
   useEffect(() => {
     const generateTicketID = () => {
@@ -33,11 +27,8 @@ const HorsRasingTiket = forwardRef((props, ref) => {
     setTicketID(generateTicketID());
   }, []);
 
- 
-
   useEffect(() => {
     if (newBette && Object.keys(newBette).length > 0) {
-      // Update bet list when a new bet is received
       setBetList((prevList) => [...prevList, newBette]);
     }
   }, [newBette]);
@@ -55,7 +46,7 @@ const HorsRasingTiket = forwardRef((props, ref) => {
             body: JSON.stringify({
               bets: betList,
               gameId: props.gameID,
-              win: false, // Assuming the initial value of win is false
+              win: false,
             }),
           });
           const data = await response.json();
@@ -73,8 +64,7 @@ const HorsRasingTiket = forwardRef((props, ref) => {
   }, [props.isTiketPrinted, props.handlePrint, betList, props.gameID]);
 
   useEffect(() => {
-    // Recalculate total bet amount whenever items or newBetAmount change
-    var calculateTotalAmount = () => {
+    const calculateTotalAmount = () => {
       const total = betList.reduce((acc, item) => acc + item.betAmount, 0);
       setTotalBetAmount(total);
     };
@@ -84,7 +74,6 @@ const HorsRasingTiket = forwardRef((props, ref) => {
 
   const deleteItem = (index) => {
     setBetList((prevItems) => {
-      // Create a new array without the item at the specified index
       const newItems = [
         ...prevItems.slice(0, index),
         ...prevItems.slice(index + 1),
@@ -93,14 +82,28 @@ const HorsRasingTiket = forwardRef((props, ref) => {
     });
   };
 
+  const handleBetAmountChange = (index, newAmount) => {
+    setBetList((prevList) => {
+      const updatedList = prevList.map((bet, i) => {
+        if (i === index) {
+          return { ...bet, betAmount: newAmount };
+        }
+        return bet;
+      });
+      return updatedList;
+    });
+  };
+
+  const amountButtons = [10, 20, 30, 50, 100];
+
   return (
     <div className="mt-5" ref={ref}>
       <Card className="font-ticketing text-lg" id="ticket">
         <CardBody>
-        <div>
-            <CardTitle tag="h3" style={{ marginLeft: '10px' , fontSize:"25px" }}>3S-Betting</CardTitle>
+          <div>
+            <CardTitle tag="h3" style={{ marginLeft: '10px', fontSize: "25px" }}>3S-Betting</CardTitle>
             <div style={{ display: 'flex', justifyContent: 'center', marginTop: '10px' }}>
-            <CardTitle tag="p" style={{ marginLeft: '10px', fontSize:"18px" }}>Ticket-Number: {ticketID}</CardTitle>
+              <CardTitle tag="p" style={{ marginLeft: '10px', fontSize: "18px" }}>Ticket-Number: {ticketID}</CardTitle>
             </div>
           </div>
           <CardSubtitle tag="h6" className="mb-2 text-muted ">
@@ -111,14 +114,9 @@ const HorsRasingTiket = forwardRef((props, ref) => {
           </div>
           <ListGroup>
             {betList.map((bet, index) => {
-              console.log(bet);
-
               if (bet.selectedButtons.length >= 1) {
                 return (
-                  <ListGroupItem
-                    key={index}
-                    className="d-flex flex-column align-items-start"
-                  >
+                  <ListGroupItem key={index} className="d-flex flex-column align-items-start">
                     {bet.selectedButtons.map((selectedBtn, btnIndex) => {
                       const [rank, dogNumber] = selectedBtn;
                       let displayValue;
@@ -138,22 +136,31 @@ const HorsRasingTiket = forwardRef((props, ref) => {
                       }
 
                       return (
-                        <div
-                          key={btnIndex}
-                          className="d-flex align-items-center mb-2 w-100"
-                        >
-                          <div className="flex-grow-1">
-                            <strong>[{dogNumber}]</strong> {displayValue}
-                            <span className="ml-2">
-                              <strong>________</strong> {bet.betAmount}_____
-                            </span>
+                        <div key={btnIndex} className="d-flex flex-column align-items-start mb-2 w-100">
+                          <div className="d-flex align-items-center w-100">
+                            <div className="flex-grow-1">
+                              <strong>[{dogNumber}]</strong> {displayValue}
+                              <span className="ml-2">
+                                <strong>________</strong> {bet.betAmount}_____
+                              </span>
+                            </div>
+                            <div className="ml-2">
+                              <FaTrash
+                                onClick={() => deleteItem(index)}
+                                style={{ cursor: "pointer", marginRight: "10px", color: "red" }}
+                              />
+                            </div>
                           </div>
-                          <div className="ml-2">
-                            <FaTrash
-                              onClick={() => deleteItem(index, btnIndex)}
-                              style={{ cursor: "pointer", marginRight: "10px", color: "red" }}
-                            />
-                            {/* <FaEdit onClick={() => openModal(index, btnIndex)} style={{ cursor: 'pointer' }} /> */}
+                          <div className="d-flex mt-2">
+                            {amountButtons.map((amount) => (
+                              <button
+                                key={amount}
+                                className="btn btn-sm btn-outline-primary ml-1"
+                                onClick={() => handleBetAmountChange(index, amount)}
+                              >
+                                {amount}
+                              </button>
+                            ))}
                           </div>
                         </div>
                       );
@@ -161,20 +168,14 @@ const HorsRasingTiket = forwardRef((props, ref) => {
                   </ListGroupItem>
                 );
               }
+              return null;
             })}
           </ListGroup>
-
-          <div className="mt-3  ">
+          <div className="mt-3">
             <div>
               <strong>Total :</strong> {totalBetAmount} Birr
-              <div
-                style={{
-                  display: "flex",
-                  justifyContent: "center",
-                  marginTop: "10px",
-                }}
-              >
-                 <Barcode value={ticketID} width={2} height={30} displayValue={false} />
+              <div style={{ display: "flex", justifyContent: "center", marginTop: "10px" }}>
+                <Barcode value={ticketID} width={2} height={30} displayValue={false} />
               </div>
             </div>
             <div>
